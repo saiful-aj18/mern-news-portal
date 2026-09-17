46730cae3b43ce5eba7e465c212228af72831d63
saiful-aj18 <alexjamson118@gmail.com>
2026-09-17 19:29:09 +0600

    final touch

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/news", newsRoutes);
 
-app.get("/health", (req, res) => {
+app.get("/", (req, res) => {
   res.send("News Portal API is running...");
 });
 
